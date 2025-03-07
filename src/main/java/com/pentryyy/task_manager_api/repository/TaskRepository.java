bf175079f91116c1dd76b3898c85281e21f5b848
pentryyy <m.v.volikov@gmail.com>
2025-03-07 17:51:36 +0300

    Исправлена n+1 проблема

--- src/main/java/com/pentryyy/task_manager_api/repository/TaskRepository.java
+++ src/main/java/com/pentryyy/task_manager_api/repository/TaskRepository.java
@@ -1,5 +1,8 @@
 package com.pentryyy.task_manager_api.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,6 @@ import com.pentryyy.task_manager_api.model.Task;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
-    
+    @EntityGraph(attributePaths = {"executor", "author"})
+    Page<Task> findAll(Pageable pageable);
 }
